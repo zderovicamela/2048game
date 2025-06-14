74a729442dbf8dcc6d38e11f86c5faf5313bea1c
ivana-vukasinovic <i.vukasinovic20@gmail.com>
2025-06-14 18:50:23 +0200

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -220,3 +220,35 @@ def generate_tiles():
         tiles[f"{row}{col}"] = Tile(2, row, col)
 
     return tiles
+
+def main(window):
+    clock = pygame.time.Clock()
+    run = True
+
+    tiles = generate_tiles()
+
+    while run:
+        clock.tick(FPS)
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                break
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    move_tiles(window, tiles, clock, "left")
+                if event.key == pygame.K_RIGHT:
+                    move_tiles(window, tiles, clock, "right")
+                if event.key == pygame.K_UP:
+                    move_tiles(window, tiles, clock, "up")
+                if event.key == pygame.K_DOWN:
+                    move_tiles(window, tiles, clock, "down")
+
+        draw(window, tiles)
+
+    pygame.quit()
+
+
+if __name__ == "__main__":
+    main(WINDOW)
